df15c686436b37e85b622685dc4dcdbc2e178ce1
juuvuor <96438568+juuvuor@users.noreply.github.com>
2026-02-16 14:14:41 +0200

    Initial Commit

---
+++ CarpoolApp/CarpoolAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarpoolAppApp.swift
+//  CarpoolApp
+//
+//  Created by Juuso Vuorela on 16.2.2026.
+//
+
+import SwiftUI
+
+@main
+struct CarpoolAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarpoolApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarpoolApp
+//
+//  Created by Juuso Vuorela on 16.2.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
